7cb37eaddbfcc803170a150b7f0704cfdd119d6a
DavidMMartiro <daviddelosmartinez@gmail.com>
2021-03-03 12:42:52 +0100

    Añadido servidor web

---
+++ backend/endpoints.js
@@ -0,0 +1,20 @@
+// Imports
+var router = require("express").Router();
+
+// Base endpoint
+router.get("/", (req, res) => {
+	res.send("Welcome to our api.")
+})
+
+// Get a random number
+router.get("/random-number", (req, res) => {
+	let number = Math.floor(Math.random()*(100-1+1)+1)
+	res.json({number: number})
+})
+router.get("/hola", (req, res) => {
+	
+	res.send("Hola chicos")
+})
+
+// Export the JS file
+module.exports = router;
